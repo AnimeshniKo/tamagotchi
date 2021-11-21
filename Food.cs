7e7ace3b90442c6051cc04e5f5c98b11cd49214a
AnimeshniKo <mikhail_tetenov1@mail.ru>
2021-11-21 17:16:15 +0400

    initial commit

---
+++ Food.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagochi
+{
+    public enum Food
+    {
+        Chicken,
+        Milk,
+        Bone
+    }
+}
---
+++ IAnimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagochi
+{
+    internal interface IAnimal
+    {
+        void Eat(Food food);
+       // void KillAnimal();
+        void Clean();
+        void Play();
+        void Drink();
+
+    }
+}
